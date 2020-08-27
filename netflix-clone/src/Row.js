87337a1ac20205202f0f3c3fc9b7d9dfdb500038
Rubin <rubinradhakrishnan@gmail.com>
2020-08-27 16:35:08 +0530

    Fetch the movie posters

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,32 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    // A snippet of code which runs on specific conditions/variable
-    useEffect(() => {
-        // if [], run once when the row loads, and dont run again
-        //unless there is a dependency for eg: [movies], so everytime "movies" variable is updated, its gonna run this portion
-        async function fetchData() {
-            // await tells to wait for the promise
-            const request = await axios.get(fetchUrl);
-            console.log(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, []);
+  //* A snippet of code which runs on specific conditions/variable
+  useEffect(() => {
+    //? if [], run once when the row loads, and dont run again
+    //* unless there is a dependency for eg: [movies], so everytime "movies" variable is updated, its gonna run this portion
+    async function fetchData() {
+      // await tells to wait for the promise
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
+  console.table(movies);
 
-    return (
-        <div>
-            <h2>{title}</h2>
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-            {/* container -> posters */}
-        </div>
-    )
+      <div className="row__posters">
+        {/* several row__posters */}
+
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
-
-
+export default Row;
